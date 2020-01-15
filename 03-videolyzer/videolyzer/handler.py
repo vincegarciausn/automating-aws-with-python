75bf1e74a8ee5002df2f511a172856bc69f405db
vincegarciausn <vincegarcia05@outlook.com>
2020-01-15 00:51:41 -0800

    added SNS and new lambda function for labeldetection

--- 03-videolyzer/videolyzer/handler.py
+++ 03-videolyzer/videolyzer/handler.py
@@ -1,5 +1,6 @@
 import urllib
 import boto3
+import os
 
 
 def start_label_detection(bucket, key):
@@ -11,7 +12,12 @@ def start_label_detection(bucket, key):
                 'Bucket': bucket,
                 'Name': key 
             }
+        },
+        NotificationChannel={
+            'SNSTopicArn': os.environ['REKOGNITION_SNS_TOPIC_ARN'],
+            'RoleArn': os.environ['REKOGNITION_ROLE_ARN']
         })
+              
     print (response)
     return
 
@@ -23,5 +29,10 @@ def start_processing_video(event, context):
         )
     
     print(event)
+
+    return
+
+def handle_label_detection (event, context):
+    print (event)
 
     return
